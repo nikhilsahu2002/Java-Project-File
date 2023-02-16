d0baedbe70837f39e30fd1ba8a4f81895ae0fccc
nikhilsahu2002 <death1233freak@gmail.com>
2023-02-16 16:07:39 +0530

    Table

--- Loop.java
+++ Loop.java
@@ -1,5 +1,12 @@
 public class Loop {
-    public static void main() {
-        
+    public static void main(String[] args) 
+    {
+        for (int j = 1; j <=4; j++) {
+            System.out.println(" ");
+        for (int i = 1; i <=10; i++) {
+            System.out.println(+j +"X" +i  +"=" +(i*j));
+        }
+
+    }
     }
 }
