d38a13949873c1bdad54c65cc19290428ec3a48c
nikhilsahu2002 <death1233freak@gmail.com>
2023-02-23 00:37:18 +0530

    commit

---
+++ Duplicate.java
@@ -0,0 +1,13 @@
+public class Duplicate {
+    public static void main(String[] args) {
+        int [] arr ={2,5,4,5,4,3,6,7,3};
+        
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[i]==arr[j]) {
+                    System.out.println(arr[j]);
+                }
+            }
+        }
+    }
+}
--- reverse.java
+++ reverse.java
@@ -1,6 +1,6 @@
 public class reverse {
     public static void main(String[] args) {
-        int [] arr = {2,6,8,9,4,2,5};
+        int [] arr = {2,6,8,9,4,2,5,8};
 
         for (int i = 0; i < arr.length/2; i++) {
             int temp = arr[i];
