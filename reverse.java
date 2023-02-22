88703a40e9d6a8a143215209f3fb922f66344038
nikhilsahu2002 <death1233freak@gmail.com>
2023-02-23 00:17:28 +0530

    commit

---
+++ reverse.java
@@ -0,0 +1,18 @@
+public class reverse {
+    public static void main(String[] args) {
+        int [] arr = {2,6,8,9,4,2,5};
+
+        for (int i = 0; i < arr.length/2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length -i -1];
+            arr[arr.length -i -1] =temp;
+        }
+        
+        
+        for (int j = 0; j < arr.length; j++) {
+            System.out.println(arr[j]);    
+        }
+        
+    }
+
+}
