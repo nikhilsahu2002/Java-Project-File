9129e86fece42fe846a0fb9aa044cc1bdade44a0
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2023-04-13 11:56:46 +0530

    commit

---
+++ pack/pack/ckassA.java
@@ -0,0 +1,8 @@
+package pack;
+
+public class ckassA{
+    public static void display() {
+        System.out.println("Class A");
+    }
+
+}
---
+++ packeage1.java
@@ -0,0 +1,8 @@
+import pack.ckassA;
+
+public class packeage1 {
+    public static void main(String[] args) {
+        ckassA.display();
+    }
+    
+}
