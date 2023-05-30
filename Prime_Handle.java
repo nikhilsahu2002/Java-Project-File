858ef509ab1a014a7bce6bd03a2844549ce10339
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2023-05-30 20:19:22 +0530

    commit

--- Prime_Handle.java
+++ Prime_Handle.java
@@ -1,35 +1,69 @@
 import java.util.Scanner;
 
-class Nikhil extends Exception {
-    public Nikhil(String message) {
+class NikhilException extends Exception {
+    public NikhilException(String message) {
         super(message);
     }
 }
 
-public class Prime_Handle {
-    public static void CheckPrime(int number) throws Nikhil {
+class PrimeChecker implements Runnable {
+    private int number;
+    private int threadNumber;
+
+    public PrimeChecker(int number, int threadNumber) throws NikhilException {
         if (number < 2) {
-            throw new Nikhil("Not Prime: " + number);
+            throw new NikhilException(number + " is not Prime");
         }
-        
-        for (int i = 2; i <= number/2; i++) {
+        this.number = number;
+        this.threadNumber = threadNumber;
+    }
+
+    @Override
+    public void run() {
+        try {
+            checkPrime(number);
+            System.out.println("Thread " + threadNumber + ": " + number + " is Prime");
+        } catch (NikhilException e) {
+            System.out.println("Thread " + threadNumber + ": " + e.getMessage());
+        }
+    }
+
+    private static void checkPrime(int number) throws NikhilException {
+        for (int i = 2; i <= Math.sqrt(number); i++) {
             if (number % i == 0) {
-                throw new Nikhil("Not Prime: " + number);
+                throw new NikhilException(number + " is not Prime");
             }
         }
-        
-        System.out.println("Number Is Prime");
     }
-    
+}
+
+public class Prime_Handle {
     public static void main(String[] args) {
-        System.out.println("Enter The Prime Number ");
-        Scanner scanner = new Scanner (System.in);
-        int number = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+
+        int number = 0;
+        boolean validNumber = false;
+        while (!validNumber) {
+            System.out.print("Enter a number: ");
+            number = scanner.nextInt();
+
+            if (number <= 10) {
+                validNumber = true;
+            } else {
+                System.out.println("Number greater than 10. Please try again.");
+            }
+        }
+
+        Thread[] threads = new Thread[number];
         try {
-            CheckPrime(number);
-        } catch (Nikhil e) {
+            for (int i = 0; i < number; i++) {
+                threads[i] = new Thread(new PrimeChecker(number, i + 1));
+                threads[i].start();
+            }
+        } catch (NikhilException e) {
             System.out.println(e.getMessage());
         }
+
         scanner.close();
     }
 }
