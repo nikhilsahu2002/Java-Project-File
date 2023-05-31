51dd7a95d0ab9aee1d3617095e029e77378f3aa5
nikhilsahu2002 <117375980+nikhilsahu2002@users.noreply.github.com>
2023-05-31 20:38:26 +0530

    insertion

---
+++ InsertionSort.java
@@ -0,0 +1,34 @@
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 9, 4, 2, 1, 6, 2, 8};
+
+        System.out.println("Original Array:");
+        printArray(arr);
+
+        insertionSort(arr);
+
+        System.out.println("Sorted Array:");
+        printArray(arr);
+    }
+
+    public static void insertionSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+}
